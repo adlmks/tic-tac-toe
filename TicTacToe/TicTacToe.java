e7341849bb62ff2b01bdd25195fbb3a915d18b43
Aidar <aidarsoulsov@mail.ru>
2024-11-06 14:50:14 +0500

    Facade

---
+++ TicTacToe/TicTacToe.java
@@ -0,0 +1,61 @@
+package TicTacToe;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import TicTacToeAI.TicTacToeAI;
+import TicTacToeMultiplayer.TicTacToeMultiplayer;
+
+
+public class TicTacToe extends JFrame {
+    public TicTacToe() {
+        setTitle("Выбор режима игры");
+        setSize(300, 200);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setLocationRelativeTo(null);
+        setLayout(new GridLayout(3, 1));
+
+        JButton singlePlayerButton = new JButton("Одиночная игра (с ИИ)");
+        JButton multiplayerButton = new JButton("Парная игра");
+        JButton exitButton = new JButton("Выход");
+
+        singlePlayerButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String playerName = JOptionPane.showInputDialog("Введите имя игрока:");
+                new TicTacToeAI(playerName).setVisible(true);
+                dispose(); // Закрыть меню выбора
+            }
+        });
+
+        multiplayerButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                // Ввод имен игроков
+                String player1 = JOptionPane.showInputDialog("Введите имя первого игрока:");
+                String player2 = JOptionPane.showInputDialog("Введите имя второго игрока:");
+
+                // Запуск многопользовательской игры
+                new TicTacToeMultiplayer(player1, player2).setVisible(true);
+                dispose(); // Закрыть меню выбора
+            }
+        });
+
+        exitButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                System.exit(0);
+            }
+        });
+
+        add(singlePlayerButton);
+        add(multiplayerButton);
+        add(exitButton);
+    }
+
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(() -> new TicTacToe().setVisible(true));
+    }
+}
